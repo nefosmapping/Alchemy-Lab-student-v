3b4a198a7de084f16b8ab1daba3f1847c4635208
nefosmapping <nefosmapping@gmail.com>
2026-04-21 18:10:46 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+"""The general configuration of the game"""
+
+# settings
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+FPS = 120
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
+BLUE = (0, 100, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# game states
+STATE_MENU = "menu"
+STATE_GAME = "game"
+
+images = {
+    1 : "assets/images/enemy.png",
+    2 : "assets/images/enemy2.png",
+    3 : "assets/images/enemy3.png"
+}
